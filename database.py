825c52c81aee38ff27e90df962de462462a8f05f
Nisarg-7 <rathodnisarg27@gmail.com>
2025-11-11 11:56:16 +0530

    Updated database connection and requirements

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-db_url = os.getenv("DATABASE_URL")
+db_url = "postgresql://app_45n0_user:bwTFSxzAVNKqJAI3qEbMzX6gh16Lw5FQ@dpg-d48su16r433s73a9707g-a.oregon-postgres.render.com/app_45n0"
 
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
